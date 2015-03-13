eb3f61c6f8ab85e3e6685527672a775f50a00461
albertgrala <albertgrala@gmail.com>
2015-03-13 23:55:18 +0100

    Fix indentation

--- tests/PcComponentesCrawlerTest.php
+++ tests/PcComponentesCrawlerTest.php
@@ -7,29 +7,29 @@ use Albertgrala\Crawlers\PcComponentesCrawler;
 class PcComponentesCrawlerTest extends \PHPUnit_Framework_TestCase
 {
 
-	protected function setUp()
-	{
-		$url = 'http://www.pccomponentes.com/kingston_valueram_4gb_ddr3_1333mhz_pc3_10600_cl9.html';
-		$this->crawler = new PcComponentesCrawler($url);
-	}
+    protected function setUp()
+    {
+        $url = 'http://www.pccomponentes.com/kingston_valueram_4gb_ddr3_1333mhz_pc3_10600_cl9.html';
+        $this->crawler = new PcComponentesCrawler($url);
+    }
 
-	protected function tearDown()
-	{
+    protected function tearDown()
+    {
 
-	}
+    }
 
-	public function testCrawlsUrl()
-	{
-		$this->crawler->crawl();
-		$this->isTrue($this->crawler->isCrawled());
-	}
+    public function testCrawlsUrl()
+    {
+        $this->crawler->crawl();
+        $this->isTrue($this->crawler->isCrawled());
+    }
 
 
-	public function testExtractPriceReturnsAndInteger()
-	{
-		$this->crawler->crawl();
-		$this->crawler->extractPrice();
+    public function testExtractPriceReturnsAndInteger()
+    {
+        $this->crawler->crawl();
+        $this->crawler->extractPrice();
         $this->assertInternalType('int', $this->crawler->getPrice());
-	}
+    }
 
 }
